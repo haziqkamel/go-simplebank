ada459c5f2a97abb6263e06b7219ba12bc79a790
Haziq Kamel <haziqkamel@outlook.com>
2024-04-13 20:43:50 +0800

    Add test for updating account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -47,3 +47,22 @@ func TestGetAccount(t *testing.T) {
 	require.Equal(t, account1.ID, account2.ID)
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, arg.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, account1.ID, account2.ID)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
