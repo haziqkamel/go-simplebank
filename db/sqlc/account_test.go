3076af81e7bed841361df53ba543260604f9324e
Haziq Kamel <haziqkamel@outlook.com>
2024-04-13 20:47:08 +0800

    Add test for listing accounts

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -78,3 +78,22 @@ func TestDeleteAccount(t *testing.T) {
 	require.EqualError(t, err, sql.ErrNoRows.Error())
 	require.Empty(t, account2)
 }
+
+func TestListAccount(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
