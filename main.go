c1ded773d46d6414af990eb85caf5d650878247d
Haziq Kamel <haziqkamel@outlook.com>
2024-04-16 22:01:27 +0800

    install go mod viper

--- main.go
+++ main.go
@@ -6,20 +6,19 @@ import (
 
 	"github.com/haziqkamel/simplebank/api"
 	db "github.com/haziqkamel/simplebank/db/sqlc"
+	"github.com/haziqkamel/simplebank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	// DefaultPort is the default port the server will listen on
-	DefaultPort   = "8080"
-	serverAddress = "localhost"
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -27,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress + ":" + DefaultPort)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
